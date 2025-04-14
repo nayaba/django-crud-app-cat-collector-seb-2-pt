7c59eeb84937bdcbf84dfcfcc7e892587462fc39
Khalil Khunji <khalilkhunji@gmail.com>
2025-04-14 20:51:39 +0300

    Made a Home Page with CSS

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     # Routes will be added here
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('cats/', views.cat_index, name='cat-index'),
 ]

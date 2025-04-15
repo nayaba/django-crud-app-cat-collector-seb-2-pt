4f922cdd60768098aa6c62c0ae92e3f8dc2e7584
Nabila Ayaba <nabilaayaba@gmail.com>
2025-04-15 20:26:11 +0300

    Re-write index view and create detail view

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,5 @@
 from django.shortcuts import render
-# Create your views here.
-
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-# Create a list of Cat instances
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
-
+from .models import Cat
 
 def home(request):
     return render(request, 'home.html')
@@ -24,4 +8,9 @@ def about(request):
     return render(request, 'about.html')
 
 def cat_index(request):
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})

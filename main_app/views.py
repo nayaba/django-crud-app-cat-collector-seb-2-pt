38cfdbe844c0b3ca2e4714e4709d6393db5e327a
Nabila Ayaba <nabilaayaba@gmail.com>
2025-04-20 19:05:31 +0300

    Full CRUD on Cats

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
 
 def home(request):
@@ -20,3 +20,11 @@ class CatCreate(CreateView):
     model = Cat
     fields = '__all__'
     # success_url = '/cats/'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'

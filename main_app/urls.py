b97fb07c37daa554a4393691df86aa61c132b219
Nabila Ayaba <nabilaayaba@gmail.com>
2025-04-21 20:17:00 +0300

    Finish auth

--- main_app/urls.py
+++ main_app/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('cats/<int:pk>/update/', views.CatUpdate.as_view(), name='cat-update'),
     path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name='cat-delete'),
     path('cats/<int:cat_id>/add-feeding', views.add_feeding, name='add-feeding'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
